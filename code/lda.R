6b35268b767f9229265177185ccc0af9a1f63e29
ribes96 <aribes96@gmail.com>
2017-11-05 21:39:45 +0100

    Arreglado problema con Fisher

--- code/lda.R
+++ code/lda.R
@@ -1,9 +1,9 @@
+library(MASS)
 aa = lda(thoraric[c(1:16)], thoraric[,"DIED"])
 negatives = thoraric[thoraric[,"DIED"] == F,]
 mean.true = aa$means[2,]
 mean.false = aa$means[1,]
 
-subset(expr, cell_type == "hesc")
 
 library(dplyr)    
 filter(thoraric, thoraric$DIED==F)
--- code/readData.R
+++ code/readData.R
@@ -22,5 +22,14 @@ names(thoraric) = c(
   "AGE",
   "DIED"
   )
-
-
+thoraric$PAIN = as.logical(thoraric$PAIN)
+thoraric$HAEMOPTYSIS = as.logical(thoraric$HAEMOPTYSIS)
+thoraric$DISPNOEA = as.logical(thoraric$DISPNOEA)
+thoraric$COUGH = as.logical(thoraric$COUGH)
+thoraric$WEAKNESS = as.logical(thoraric$WEAKNESS)
+thoraric$DIABETES = as.logical(thoraric$DIABETES)
+thoraric$MI = as.logical(thoraric$MI)
+thoraric$PAD = as.logical(thoraric$PAD)
+thoraric$SMOKE = as.logical(thoraric$SMOKE)
+thoraric$ASTHMA = as.logical(thoraric$ASTHMA)
+thoraric$DIED = as.logical(thoraric$DIED)
