f6cb9e49ba47fa30debc39b6afc7f000998680ed
ribes96 <aribes96@gmail.com>
2017-12-03 15:40:22 +0100

    Ended (but not tested) the general cross-validation function

--- code/crossValidation.R
+++ code/crossValidation.R
@@ -1,5 +1,8 @@
 # Runs a cross validation for a general model
 
+
+# I still have not tested this function
+# It may not match with every possible model
 custom.cv = function(df, model, nfolds, params) {
   # model is a function to be called with the elements of params as parameters
   # the data will be added to the params
@@ -13,8 +16,27 @@ custom.cv = function(df, model, nfolds, params) {
   
   solutions = dataFrame[,"DIED"]
   data = dataFrame[,-14] # 14 corresponds to row "DIED"
+  totalErrors = 0
+  maxPosErrors = nrow(dataFrame)
   for (i in 1:nfolds) {
-    data.train = data[,]
+    if (i == nfolds)
+      testIndex = fold.size*(i - 1) + 1:nrow(dataFrame)
+    else
+      testIndex = fold.size*(i - 1) + 1:fold.size*i
+    # The last one may have less elements
+    data.test = data[-testIndex,]
+    data.train = data[testIndex,]
+    data.results = solutions[-testIndex]
+    
+    pred = model(c(list(train = data.train, test = data.test, cl = data.results), params))
+    
+    pred.log = as.logical(pred)
+    
+    equality = pred.log == data.results
+    
+    nerrors = length(equality[equality == F])
+    
+    totalErrors = totalErrors + nerrors
   }
-  
+  return(totalErrors/maxPosErrors)
 }
