22d352e937cbda372b9159e67407d5ba68019129
ribes96 <aribes96@gmail.com>
2017-12-11 11:45:11 +0100

    Corregido el Linear Model y avanzado un poco

--- code/genLinMod.R
+++ code/genLinMod.R
@@ -14,8 +14,12 @@ learn = sample(1:n, 2*n/3)
 alll = 1:n
 test = alll[-learn]
 
-df = as.numeric(df)
+#df = as.numeric(unlist(df))
+df = as.data.frame(apply(df, MARGIN = c(1,2), FUN = as.numeric))
 df$DIED = as.factor(df$DIED)
+
+
+
 
 ## specify 10x10 CV
 trc <- trainControl (method="repeatedcv", number=10, repeats=10)
@@ -36,10 +40,17 @@ mod.glm = glm(DIED ~ ., data = df, family = binomial(link = "logit"))
 
 test_pred <- predict(mod.glm, newdata = df[test,], type = "response")
 
+# Prediction tiene la predicción hecha para cada uno de los testin
+prediction = rep(TRUE, length(test_pred))
+prediction[test_pred < 0.5] = FALSE
+
+real_pred = df[test, 14]
+
+
 test_pred
 
-p = exp(test_pred)/(1-exp(test_pred))
-length(p[p>1 | p<0])
-length(p[p>0.5 & p<1])
+#p = exp(test_pred)/(1-exp(test_pred))
+#length(p[p>1 | p<0])
+#length(p[p>0.5 & p<1])
 
 confusionMatrix(test_pred, df[test,]$DIED )
