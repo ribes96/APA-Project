c6b839750b5e38afa617c515b8a609bb86c7d11a
ribes96 <aribes96@gmail.com>
2017-12-11 08:08:56 +0100

    Terminado de normalizar y escrito coding of non-continuous variables

--- code/normalize.R
+++ code/normalize.R
@@ -1,5 +1,5 @@
 
-margin = 0.2
+margin = 0.1
 
 FEV1.max = (1 + margin) * max(thoraric$FEV1)
 FEV1.min = (1 - margin) * min(thoraric$FEV1)
@@ -7,7 +7,17 @@ FEV1.min = (1 - margin) * min(thoraric$FEV1)
 FVC.max = (1 + margin) * max(thoraric$FVC)
 FVC.min = (1 - margin) * min(thoraric$FVC)
 
+AGE.max = 100
+AGE.min = 0
+
+PERFORMANCE.max = max(thoraric$PERFORMANCE)
+PERFORMANCE.min = min(thoraric$PERFORMANCE)
+
 thoraric$FVC = (thoraric$FVC - FVC.min)/(FVC.max - FVC.min)
 thoraric$FEV1 = (thoraric$FEV1 - FEV1.min)/(FEV1.max - FEV1.min)
+
+thoraric$AGE = (thoraric$AGE - AGE.min)/(AGE.max - AGE.min)
+
+thoraric$PERFORMANCE = (thoraric$PERFORMANCE - PERFORMANCE.min)/(PERFORMANCE.max - PERFORMANCE.min)
 
 #TODO queda normalizar AGE, y quizá SIZE y PERFORMANCE
