f7e36dfd56adedc1e62f23da6c4f3491aa02f613
Kerstin <winterke@students.uni-marburg.de>
2018-01-16 19:04:59 +0100

    RData con todos los modelos. Introducciones de redes neuronales y random forest.

--- code/Models/rf.R
+++ code/Models/rf.R
@@ -21,6 +21,5 @@ getRF.model = function(df) {
 #retorna una lista de modelos random forest, que tiene el mismo tamaño que la lista de entrada
 getSuper.rf = function(dflist) {
   models.list = lapply(dflist, getRF.model)
-  super.RF <<- models.list
   return(models.list)
 }
